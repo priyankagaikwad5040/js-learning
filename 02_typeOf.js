f010e8b5445c4f567ad616d07a54eaf67a398f24
priyankagaikwad5040 <priyankagaikwad5040@gmail.com>
2024-03-09 08:58:19 +0530

    datatype
    datatype

---
+++ 02_typeOf.js
@@ -0,0 +1,35 @@
+var bankName;
+console.log("---------------------Before Initialization--------------");
+console.log("bank name value before initialization             -",bankName);
+console.log("Data Type of bank Name before initialization      -",typeof bankName);
+bankName="SBI Bank";
+console.log("---------------------After Initialization--------------");
+console.log("bank name value after initialization              -",bankName);
+console.log(" Data Type of bank Name after initialization      -",typeof bankName);
+console.log("----------------------------------------------------------");
+var marks='90%'; 
+var typeofMark=typeof marks;
+console.log("The value of marks                                -",marks);
+console.log("The  data type of  marks                          -",typeofMark);
+console.log("----------------------------------------------------------");
+var isWorking;
+var typeofWork=typeof isWorking;
+console.log("---------------------Before Initialization--------------");
+console.log("Working status value before initialization        -",isWorking);
+console.log(" Data Type of Working before initialization       -",typeof typeofWork);
+isWorking=true;
+console.log("---------------------After Initialization--------------");
+console.log("Working Status value after initialization         -",isWorking);
+console.log(" Data Type of Working after initialization        -",typeof typeofWork);
+console.log("----------------------------------------------------------");
+var totalCount="Hundred and Seven";
+console.log("The Value of Total Count                          -",totalCount);
+console.log("Data type of total Count                          -",typeof totalCount);
+totalCount=107;
+console.log("---------------------After Updating--------------");
+console.log("The Value of Total Count                          -",totalCount);
+console.log("Data type of total Count                          -",typeof totalCount);
+
+
+
+
